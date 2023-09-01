36e0cba6f1b40283266626aa0a2af9fd1a8e3727
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-09-01 13:31:51 +0200

    fixed imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Card from "./assets/components/card/card";
 import Layout from "./assets/layout/layout";
+import projImg from "./assets/projImg1.jpeg";
+import projImg2 from "./assets/projImg2.jpeg";
 import "./index.css";
 
 function App() {
@@ -8,19 +10,19 @@ function App() {
       <Layout>
         <Card
           name="This project"
-          imgURL="./src/assets/projImg1.PNG"
+          imgURL={projImg}
           description='This "beautiful" project here'
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
         <Card
           name="Some other project"
-          imgURL="./src/assets/projImg1.PNG"
+          imgURL={projImg2}
           description="This other project"
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
         <Card
           name="Definetely not the same project"
-          imgURL="./src/assets/projImg1.PNG"
+          imgURL={projImg2}
           description="This absolutely-different-from-the-other-two project"
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
