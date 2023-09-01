88805fa33fe67504d7c3de50c9935575b9b29b2c
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-09-01 13:11:45 +0200

    wth

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ function App() {
       <Layout>
         <Card
           name="This project"
-          imgURL="/assets/projImg1.PNG"
+          imgURL="./src/assets/projImg1.PNG"
           description='This "beautiful" project here'
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
         <Card
           name="Some other project"
-          imgURL="/assets/projImg1.PNG"
+          imgURL="./src/assets/projImg1.PNG"
           description="This other project"
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
         <Card
           name="Definetely not the same project"
-          imgURL="/assets/projImg1.PNG"
+          imgURL="./src/assets/projImg1.PNG"
           description="This absolutely-different-from-the-other-two project"
           link="https://github.com/Kristoffer-Kodehode/frameProj1"
         ></Card>
